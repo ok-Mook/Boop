c9b3b7d78989cf6c72a3bf294fbe6086daa2911f
artkumik <artkumik@gmail.com>
2024-04-11 14:57:15 -0400

    Update Git.py

--- Git.py
+++ Git.py
@@ -7,7 +7,16 @@ def print_menu():
     print('2. Decode')
     print('3. Quit')
     c = int(input('\nPlease enter an option: '))
-
+    if c == 1:
+        number = input("Please enter the number to encode: ")
+        print(f"Your encoded number is {encode(number)}")
+    elif c == 2:
+        number = input("Please enter the number to decode: ")
+        print(f"Your decoded number is {decode(number)}")
+    elif c == 3:
+        print('Goodbye!')
+    else:
+        print("Please enter a valid option.")
 
 def encode(string):
     encoded_string = ''
@@ -20,4 +29,14 @@ def encode(string):
         encoded_string += ''.join(lis[i])
     return encoded_string
 
+def decode(user):
+    user = str(user)
+    decoding_list = [7,8,9,0,1,2,3,4,5,6]
+    decoded_user = ""
 
+    for i in user:
+        decoded_user = decoded_user + str(decoding_list[int(i)])
+
+    print(decoded_user)
+
+print_menu()
